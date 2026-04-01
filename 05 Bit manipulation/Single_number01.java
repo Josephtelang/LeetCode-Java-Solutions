87ecafaccbd8ecb86ac22dbb601332eea366b423
josephtelang <josephtelang303@gmail.com>
2026-04-02 01:42:15 +0530

    Add this problem of leetcode

---
+++ 05 Bit manipulation/Single_number01.java
@@ -0,0 +1,19 @@
+
+
+public class Single_number01 {
+    public static void main(String arg[]){
+        int arr[] = {4,1,2,1,2};
+        System.out.println("The single number in array is : "+singleNumber(arr));
+    }
+    public static int singleNumber(int[] nums) {
+        int result =0;
+        int n = nums.length;
+        for (int i =0 ; i<n ; i++){
+            result = nums[i]^result;
+        }
+        return result;
+ 
+        
+    }
+    
+}
