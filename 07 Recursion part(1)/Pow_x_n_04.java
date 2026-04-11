fdc8275f33666af4c6566666aecba8712ed1c103
josephtelang <josephtelang303@gmail.com>
2026-04-11 05:33:41 +0530

    add this problem

---
+++ 07 Recursion part(1)/Pow_x_n_04.java
@@ -0,0 +1,42 @@
+
+// Problem: Fibonacci Numbers
+// leetCode: 50        Google, Amazon
+// Difficulty: medium
+// Time Complexity: O(log n)
+// Space Complexity: O(log n)
+public class Pow_x_n_04 {
+
+    public static void main(String arg[]){
+        System.out.println(myPow(2.0,-2));
+    }
+
+
+    public static double myPow(double x, int n) {
+        return helper(x,n);
+
+    }
+
+    private static double helper(double x, long n){
+         if (n==1){
+            return x;
+        }
+        
+        if (n==0){
+            return 1;
+        }
+        if(n <=0){
+            return 1/helper(x,-n);
+        }
+    
+
+        double halfPower = helper(x,n/2);
+        double halfPowerSquare = halfPower * halfPower;
+
+        //odd
+        if(n%2 !=0){
+            halfPowerSquare = x*halfPowerSquare;
+        }
+
+        return halfPowerSquare;
+       }
+}
